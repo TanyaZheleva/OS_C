49eb163dba0442cda348461e17a47f4c92266f16
TanyaZheleva <44448449+TanyaZheleva@users.noreply.github.com>
2020-07-02 23:18:31 +0300

    fix solution

--- file.input.output/11/main.c
+++ file.input.output/11/main.c
@@ -15,58 +15,46 @@ int cmp(const void *a, const void *b){
 int main(int argc, char *argv[]){
 	
 	if( argc !=2 ){
-		errx(1,"Only one argument\n");
+		errx(1,"Only one argument");
 	}
-	int fd1;
-	if ((fd1=open(argv[1],O_RDONLY)) == -1){
-		err(2,"File failed to open in read mode\n");
-	}
+	
 	struct stat st;
 	if(stat(argv[1],&st) == -1){
-		const int _errno = errno;
-                close(fd1);
-                errno=_errno;
-                err(3,"Failed to stat\n");
+                err(2,"Failed to stat\n");
 	}
-	if(st.st_size == 0){ //if source file is empty
-		close(fd1);
+	if(st.st_size == 0){
 		exit(0);
 	}
-
-	int fd2;
-        if ((fd2=open("sorted_file", O_CREAT | O_WRONLY | O_TRUNC, S_IWUSR | S_IRUSR))== -1){
-                const int _errno = errno;
-                close(fd1);
-                errno=_errno;
-                err(4,"Failed to open file in write mode\n");
-        }
+	
+	int fd;
+	if ((fd=open(argv[1],O_RDWR)) == -1){
+		err(3,"File failed to open");
+	}
+	
 	void *buff = malloc(st.st_size);
 	if (!buff){
 		const int _errno = errno;
                 close(fd1);
                 close(fd2);
                 errno=_errno;
-                err(5,"No memory\n");
+                err(4,"No memory\n");
 	}
-	if ( read(fd1,buff,st.st_size) != st.st_size ){
+	if ( read(fd,buff,st.st_size) != st.st_size ){
 		const int _errno = errno;
-		close(fd1);
-                close(fd2);
+		close(fd);
 		free(buff);
                 errno=_errno;
-                err(6,"Failed reading\n");
+                err(5,"Failed reading\n");
 	}
 	qsort(buff,st.st_size,1,cmp);
-	if(write(fd2,buff,st.st_size) != st.st_size){
+	if(write(fd,buff,st.st_size) != st.st_size){
 		const int _errno = errno;
-                close(fd1);
-                close(fd2);
+                close(fd);
                 free(buff);
                 errno=_errno;
-                err(7,"Failed writing\n");
+                err(6,"Failed writing\n");
 	}
 	free(buff);
-	close(fd1);
-	close(fd2);
+	close(fd);
 	exit (0);
 }
