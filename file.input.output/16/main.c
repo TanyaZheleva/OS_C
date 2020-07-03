76dd7dce28903d900dffcb3214e6f626271b7b75
TanyaZheleva <44448449+TanyaZheleva@users.noreply.github.com>
2020-07-04 00:15:17 +0300

    update solution

--- file.input.output/16/main.c
+++ file.input.output/16/main.c
@@ -5,107 +5,67 @@
 име на файл - се приема за STDIN
 при липса на подадена опция просто се извежда съдържанието на файловете последователно
 */
-
+#include <unistd.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 #include <err.h>
 #include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]){
-	//if no arguments are given or the only argument is '-' -> read stdin and print on stdout
-	if(argc==1 || (argc == 2 && strcmp(argv[1], "-") == 0)){
-		char c;
-		while(read(0,&c,sizeof(c)) > 0){
-			if(write(1,&c,sizeof(c)) != sizeof(c)){
-				err(1,"Failed to write to stdout");
-			}
-		}
-		exit(0);
+	int counting=0;
+	int i=1;
+	
+	if(argc != 1 && strcmp(argv[1],"-n") == 0){
+		counting=1;
+		i++;
 	}
 	
-	//if the only argument is -n -> read stdin, numerate line and print on stdout
-	if(argc == 2 && strcmp(argv[1], "-n") == 0){
-		char c;
-		int newline=1;
-		int line=1;//count lines
-		while(read(0,&c,sizeof(c)) > 0){
-			if(newline == 1){
-				setbuf(stdout,NULL);
-				fprintf(stdout,"%02d",line);
-				newline=0;
-			}
-			if(c == '\n'){
-				newline=1;
-				line++;
-			}
-			if(write(1,&c,sizeof(c)) != sizeof(c)){
-				err(2,"Failed to write to stdout");
+	while(i<argc || argc==1 || (argc==2 && counting == 1) ){
+		int fd=0;
+		
+		if(argc != 1 && ((counting==1 && argc!=2) || counting == 0 ) &&  strcmp(argv[i],"-") != 0){
+			fd=open(argv[i],O_RDONLY);
+			if(fd == -1){
+				err(10,"Fail open");
 			}
 		}
-		exit(0);
-	}
-	
-	//if there is  one argument (and its not -n) or more 
-	//check for numbeerring of lines
-	const char n = (strcmp(argv[1],"-n") == 0) ? 2 : 1; // if -n is set n is 2 because the first argument is -n
-	int rd;//read from
-	int numline=1;//count lines
-	int wd =1;//write to
-	char c;//buffer
-	int check=1;//in case -n is set; to find newlines
-	for (int i=n;i<argc;i++){
-		if(strcmp(argv[i],"-") == 0){
-			rd=0;//read from stdin
-			wd=1;//write to stdout
+		char c;
+		if(counting == 1){
+			int lines=1;
+			int prnl=1;
+			while(read(fd,&c,sizeof(c)) == sizeof(c)){
+				if(prnl == 1){
+					setbuf(stdout,NULL);
+					fprintf(stdout,"%02d",lines);
+					lines++;
+					prnl=0;	
+				}					
+				if(write(1,&c,sizeof(c)) != sizeof(c)){
+					err(i*3,"Fail wrritii");
+				}
+				if(c=='\n'){
+					prnl=1;
+				}
+			}
 		}
 		else{
-			rd=open(argv[i],O_RDONLY);
-			if(rd<0){
-				err(1,"File %s failed to open",argv[i]);
-			}
-		}
-
-		if (n==1){
-			while(read(rd,&c,sizeof(c)) > 0){
-				if(write(wd,&c,sizeof(c)) < 0){
-					const int _errno=errno;
-					close(rd);
-					close(wd);
-					errno=_errno;
-					err(2,"Failed to write");
-				}
-			}
-			close(rd);
-			close(wd);
-		}
-		else{//print with numbering
-			while(read(rd,&c,sizeof(c)) > 0){
-				if(check == 1){
-					setbuf(stdout,NULL);
-					fprintf(stdout,"%02d",numline);
-					check=0;
-				}
-
-				if( c == '\n' ){
-					check=1;
-					numline++;
-				}
-
-				if(write(wd,&c,sizeof(c)) != sizeof(c)){
-					const int _errno=errno;
-					close(rd);
-					errno=_errno;
-					err(5, "Failed writing to stdout");
+			while(read(fd,&c,sizeof(c)) == sizeof(c)){
+				if(write(1,&c,sizeof(c))!=sizeof(c)){
+					err(i*2,"Failed to write");
 				}
 			}
 		}
+
+		if(fd != 0){
+			close(fd);
+		}
+
+		i++;
 	}
-	
 	exit(0);
 }
