bfcb7468048215a771330c10c3af67e2152b28d8
TanyaZheleva <44448449+TanyaZheleva@users.noreply.github.com>
2020-07-07 21:58:16 +0300

    rm some includes

--- processes/11/main.c
+++ processes/11/main.c
@@ -5,11 +5,8 @@
 
 #include <err.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[]){
